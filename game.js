2a8c1c3112bc8f65acb6a64bffc0ba49be5c47bc
yelena guliyeva <gulbadam@gulbadam.com>
2018-07-17 14:49:03 -0700

    reset

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton=document.querySelector("#reset");
 colorDisplay.textContent = pickedColor;
 for (var i = 0; i < colors.length; i++) {
     squares[i].style.backgroundColor = colors[i] ;
@@ -12,6 +13,7 @@ for (var i = 0; i < colors.length; i++) {
         var clickedColor = this.style.backgroundColor;
         if(clickedColor===pickedColor) {
             messageDisplay.textContent="Correct!";
+            resetButton.textContent="Play again!";
             changeColors(clickedColor);
             h1.style.backgroundColor=clickedColor;
         }else{
@@ -20,6 +22,27 @@ for (var i = 0; i < colors.length; i++) {
         }
     });
 }
+resetButton.addEventListener("click", function() {
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < colors.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].addEventListener("click", function () {
+            var clickedColor = this.style.backgroundColor;
+            if (clickedColor === pickedColor) {
+                messageDisplay.textContent = "Correct!";
+                resetButton.textContent = "Play again!";
+                changeColors(clickedColor);
+                h1.style.backgroundColor = clickedColor;
+            } else {
+                this.style.backgroundColor = "#232323";
+                messageDisplay.textContent = "Try again";
+            }
+        });
+    }
+    
+})
 function changeColors(color) {
     for (var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor= color;
